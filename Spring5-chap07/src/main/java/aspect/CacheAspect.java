15eb45f62ff1b4fb8a8d1b08d6aa9d821087c438
Hong <fheldgktpdy@khu.ac.kr>
2021-07-23 12:47:53 +0900

    chap07

---
+++ Spring5-chap07/src/main/java/aspect/CacheAspect.java
@@ -0,0 +1,14 @@
+package aspect;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class CacheAspect {
+
+}
